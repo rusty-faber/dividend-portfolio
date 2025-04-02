151991e4d633bfda654c99fc662cf525d4e34723
rusty-faber <rusty_faber@lucky.zaq.jp>
2025-04-02 11:02:03 +0900

    Changes to be committed: 	modified:   app/Main.hs 	new file:   stack.yaml.lock 	new file:   test.txt
     Github Copilot
     + "Modify the program to accept a file name as a command line argument."
     + "Add a function to read the contents of a file and print it to the console."

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main (main) where
 
+import System.Environment (getArgs)
+import System.IO (readFile)
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    case args of
+        [fileName] -> do
+            content <- readFile fileName
+            putStrLn $ "File contents:\n" ++ content
+        _          -> putStrLn "Usage: program <filename>"
