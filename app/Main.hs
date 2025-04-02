d7e0b31d2e28bb98b9a67d9cd0a14e46fdf0572c
rusty-faber <rusty_faber@lucky.zaq.jp>
2025-04-02 15:58:58 +0900

    Changes to be committed: 	modified:   app/Main.hs 	new file:   output.csv 	modified:   src/Lib.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,12 @@
 module Main (main) where
 
 import System.Environment (getArgs)
-import System.IO (readFile)
 import Lib
 
 main :: IO ()
 main = do
     args <- getArgs
-    if length args == 1
-        then printFileContents (head args)
-        else putStrLn "Usage: program <filename>"
+    case args of
+        [inputFile, outputFile] -> convertXml2Csv inputFile outputFile
+        [filePath] -> printFileContents filePath
+        _ -> putStrLn "Usage: program <filename> or program <inputFile> <outputFile>"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,27 +1,42 @@
+{-# LANGUAGE Arrows #-}
+
 module Lib
     ( printFileContents,
       convertXml2Csv
     ) where
 
+import Control.Arrow
 import Text.XML.HXT.Core
-import Data.List (intersperse)
+import Data.List (intercalate)
 
 printFileContents :: FilePath -> IO ()
 printFileContents filePath = do
     content <- readFile filePath
     putStrLn content
-printFileContents _ = putStrLn "File not found."
 
 convertXml2Csv :: FilePath -> FilePath -> IO ()
 convertXml2Csv inputFile outputFile = do
-    csvData <- runX (readDocument [withValidate no] inputFile >>> processXmlToCsv)
-    writeFile outputFile (unlines csvData)
+    debugData <- runX (readDocument [withValidate no, withInputEncoding utf8] inputFile >>> writeDocumentToString [])
+    putStrLn "Debugging XML Content:"
+    mapM_ putStrLn debugData  -- XMLの内容をデバッグ出力
+    csvData <- runX (readDocument [withValidate no, withInputEncoding utf8] inputFile >>> processXmlToCsv)
+    let header = "Symbol,Name,Shares,PurchasePrice,CurrentPrice"
+    writeFile outputFile (unlines (header : csvData))
     return ()
 
 processXmlToCsv :: ArrowXml a => a XmlTree String
 processXmlToCsv =
-    deep (isElem >>> hasName "record") >>>  -- Adjust "record" to match your XML structure
-    listA (getChildren >>> getText) >>>
-    arr (concat . intersperse ",")
+    deep (isElem >>> hasName "Stock") >>> 
+    proc stock -> do
+        symbol <- getChildText "Symbol" -< stock
+        name <- getChildText "Name" -< stock
+        shares <- getChildText "Shares" -< stock
+        purchasePrice <- getChildText "PurchasePrice" -< stock
+        currentPrice <- getChildText "CurrentPrice" -< stock
+        -- デバッグ用出力
+        returnA -< intercalate "," [symbol, name, shares, purchasePrice, currentPrice]
 
-
+-- 子要素のテキストを取得するヘルパー関数
+getChildText :: ArrowXml a => String -> a XmlTree String
+getChildText tagName =
+    getChildren >>> isElem >>> hasName tagName >>> getText
