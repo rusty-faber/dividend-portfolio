7a3a9e23bf8556610b49d47fb1f9526b10380d4a
rusty-faber <rusty_faber@lucky.zaq.jp>
2025-04-07 16:12:52 +0900

    Changes to be committed: 	modified:   .gitignore 	modified:   app/Main.hs 	modified:   src/Lib.hs
        Implement code to configure the list of XML tags for extraction as a [String] type.
        This allows for more flexibility in specifying the tags to be extracted from the XML file.

--- app/Main.hs
+++ app/Main.hs
@@ -11,14 +11,26 @@ main = do
     return ()
     
 executeCommand :: [String] -> IO ()
-executeCommand [inputFile, outputFile] = do
-    inputExists <- doesFileExist inputFile
-    if inputExists
-        then convertSampleXmlToCsv inputFile outputFile
-        else putStrLn $ "Error: Input file " ++ inputFile ++ " does not exist."
-executeCommand [filePath] = do
-            fileExists <- doesFileExist filePath
-            if fileExists
-                then printFileContents filePath
-                else putStrLn $ "Error: File " ++ filePath ++ " does not exist."
+executeCommand ["-e", inputFile, outputFile] = convertInToOut convertSampleXmlToCsv inputFile outputFile
+executeCommand [inputFile, outputFile] = convertInToOut convertSampleXmlToCsv inputFile outputFile
+executeCommand [filePath] = printFile printFileContents filePath
 executeCommand _ = putStrLn "Usage: program <filename> or program <inputFile> <outputFile>"
+
+convertInToOut :: (String -> String -> IO ()) -> String -> String -> IO ()
+convertInToOut printFunc inputFile outputFile = do
+    inputExists <- doesFileExist inputFile
+    outputExists <- doesFileExist outputFile
+    if inputExists
+        then if not outputExists
+            then printFunc inputFile outputFile
+            else putStrLn $ "Error: Output file " ++ outputFile ++ " already exists."
+        else putStrLn $ "Error: Input file " ++ inputFile ++ " does not exist."
+    return ()
+
+printFile :: (String -> IO ()) -> String -> IO ()
+printFile printFunc filePath = do
+    fileExists <- doesFileExist filePath
+    if fileExists
+        then printFunc filePath
+        else putStrLn $ "Error: File " ++ filePath ++ " does not exist."
+    return ()
--- src/Lib.hs
+++ src/Lib.hs
@@ -7,7 +7,8 @@ module Lib
     ) where
 
 import Text.XML.HXT.Core
-import Data.List (intercalate)
+import Data.List (intercalate, uncons)
+import Data.Maybe (fromMaybe, listToMaybe)
 import Control.Exception (try, SomeException)
 
 printFileContents :: FilePath -> IO ()
@@ -17,37 +18,46 @@ printFileContents filePath = do
 
 convertSampleXmlToCsv :: FilePath -> FilePath -> IO ()
 convertSampleXmlToCsv inputFile outputFile = do
-    result <- try (convertXmlToCsvRunX inputFile) :: IO (Either SomeException [String])
+    let tagXML = ["Stock", "Symbol", "Name", "Shares", "PurchasePrice", "CurrentPrice"]
+    let header = intercalate "," (fromMaybe [] (snd <$> uncons tagXML))
+    result <- try (convertXmlToCsvRunX inputFile tagXML) :: IO (Either SomeException [String])
     case result of
         Left ex -> putStrLn $ "Error processing XML file: " ++ show ex
-        Right csvData -> writeCsvDocument outputFile csvData
+        Right csvData -> writeCsvDocument outputFile header csvData
 
-convertXmlToCsvRunX :: FilePath -> IO [String]
-convertXmlToCsvRunX inputFile = do
-    csvData <- runX $ readDocument [withValidate no, withRemoveWS yes] inputFile >>> processXmlToCsv
+convertXmlToCsvRunX :: FilePath -> [String] -> IO [String]
+convertXmlToCsvRunX inputFile tagXML = do
+    csvData <- runX $ readDocument [withValidate no, withRemoveWS yes] inputFile >>> 
+                      processXmlToCsv tagXML
     return csvData
 
-writeCsvDocument :: FilePath -> [String] -> IO ()
-writeCsvDocument outputFile csvData = do
-    let header = "Symbol,Name,Shares,PurchasePrice,CurrentPrice"
+writeCsvDocument :: FilePath -> String -> [String] -> IO ()
+writeCsvDocument outputFile header csvData = do
     writeFile outputFile (unlines (header : csvData))
     putStrLn "Debugging CSV Content:"
-    mapM_ putStrLn csvData  -- CSVの内容をデバッグ出力
+    mapM_ putStrLn (header : csvData)  -- CSVの内容をデバッグ出力
     return ()
 
-processXmlToCsv :: ArrowXml a => a XmlTree String
-processXmlToCsv =
-    deep (isElem >>> hasName "Stock") >>>
-    proc stock -> do
-        symbol <- getChildText "Symbol" -< stock
-        name <- getChildText "Name" -< stock
-        shares <- getChildText "Shares" -< stock
-        purchasePrice <- getChildText "PurchasePrice" -< stock
-        currentPrice <- getChildText "CurrentPrice" -< stock
-        
-        returnA -< intercalate "," [symbol, name, shares, purchasePrice, currentPrice]
+processXmlToCsv :: ArrowXml a => [String] -> a XmlTree String
+processXmlToCsv tagXML = 
+    case listToMaybe tagXML of
+        Nothing -> constA "タグが指定されていません" -- 空リストの場合のエラーメッセージ
+        Just rootTag -> deep (isElem >>> hasName rootTag) >>>
+            proc stock -> do
+                childrenXML <- getXMLChildrenText (drop 1 tagXML) -< stock
+                returnA -< childrenXML
 
 -- Helper function to retrieve the text of a child element
-getChildText :: ArrowXml a => String -> a XmlTree String
-getChildText tagName =
-    getChildren >>> isElem >>> hasName tagName >>> xshow getChildren
+getXMLChildrenText :: ArrowXml a => [String] -> a XmlTree String
+getXMLChildrenText tagXML =
+    proc stock -> do
+        texts <- listA (foldl1 (<+>) (map (\tag -> 
+            getChildren >>> 
+            isElem >>> 
+            hasName tag >>> 
+            xshow getChildren) tagXML)) -< stock
+        returnA -< intercalate "," texts
+
+-- getChildText :: ArrowXml a => String -> a XmlTree String
+-- getChildText tagName = 
+--     getChildren >>> isElem >>> hasName tagName >>> xshow getChildren
